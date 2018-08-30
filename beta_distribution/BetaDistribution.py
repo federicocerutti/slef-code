a506085ebc45abc42739b4dc7b69a681ae3ec31b
Federico Cerutti <CeruttiF@cardiff.ac.uk>
2018-08-30 12:15:36 +0100

    added a few beta operations

--- beta_distribution/BetaDistribution.py
+++ beta_distribution/BetaDistribution.py
@@ -32,13 +32,15 @@ import scipy.special
 import numpy
 import pylab
 
+from subjective_logic import Opinion
+
 class BetaDistribution():
 
     def getAlpha(self):
-        return self._alpha
+        return float(self._alpha)
     
     def getBeta(self):
-        return self._beta
+        return float(self._beta)
 
     
     def __init__(self, alpha, beta):
@@ -48,7 +50,7 @@ class BetaDistribution():
 
 
     def check(self):
-        if not(self._alpha > 0 and self._beta > 0):
+        if not(self._alpha >= 0 and self._beta >= 0):
             raise NotABetaDistributionException(self)
 
     def __repr__(self):
@@ -76,3 +78,67 @@ class BetaDistribution():
 
         pylab.plot(p, r)
         pylab.show()
+
+    def mean(self):
+        return float(self.getAlpha() / (self.getAlpha() + self.getBeta()))
+
+    def variance(self):
+        return float(self.getAlpha() * self.getBeta() / ( (self.getAlpha() + self.getBeta())**2  * (self.getAlpha() + self.getBeta() + 1) ))
+
+    def union(self, Y):
+        if not isinstance(Y, BetaDistribution):
+            raise NotABetaDistributionException(Y)
+
+        mean = self.mean() + Y.mean() - self.mean() * Y.mean()
+        var = self.variance() + 2 * Y.mean() * self.variance() + \
+            Y.variance() + 2 * self.mean() * Y.variance() + \
+            self.variance() * Y.variance() + self.variance() * (Y.mean())**2 + (self.mean())**2 * Y.variance()
+
+        alpha = max(0, mean * ((mean * (1 - mean)) / var - 1))
+        beta = max(0, (1 - mean) * ((mean * (1 - mean)) / var - 1))
+
+        return BetaDistribution(alpha, beta)
+
+    def sum(self, Y):
+        if not isinstance(Y, BetaDistribution):
+            raise NotABetaDistributionException(Y)
+
+        mean = self.mean() + Y.mean()
+        var = self.variance() + Y.variance()
+
+        alpha = max(0, mean * ((mean * (1 - mean)) / var - 1))
+        beta = max(0, (1 - mean) * ((mean * (1 - mean)) / var - 1))
+
+        return BetaDistribution(alpha, beta)
+
+    def product(self, Y):
+        if not isinstance(Y, BetaDistribution):
+            raise NotABetaDistributionException(Y)
+
+        mean = self.mean() * Y.mean()
+        var = self.variance() * Y.variance() + \
+              self.variance() * (Y.mean())**2 + Y.variance() * (self.mean()) ** 2
+
+        alpha = max(0, mean * ((mean * (1 - mean)) / var - 1))
+        beta = max(0, (1 - mean) * ((mean * (1 - mean)) / var - 1))
+
+        return BetaDistribution(alpha, beta)
+
+    def division(self, Y):
+        if not isinstance(Y, BetaDistribution):
+            raise NotABetaDistributionException(Y)
+
+        mean = scipy.special.beta(self.getAlpha() + 1, self.getBeta()) * scipy.special.beta(Y.getAlpha() - 1, Y.getBeta()) / \
+               (scipy.special.beta(self.getAlpha(), self.getBeta()) * scipy.special.beta(Y.getAlpha(), Y.getBeta()))
+        var = mean = scipy.special.beta(self.getAlpha() + 2, self.getBeta()) * scipy.special.beta(Y.getAlpha() - 2, Y.getBeta()) / \
+               (scipy.special.beta(self.getAlpha(), self.getBeta()) * scipy.special.beta(Y.getAlpha(), Y.getBeta()))
+
+        alpha = max(0, mean * ((mean * (1 - mean)) / var - 1))
+        beta = max(0, (1 - mean) * ((mean * (1 - mean)) / var - 1))
+
+        return BetaDistribution(alpha, beta)
+
+    def getOpinion(self, a =1/2, W = 1/2):
+        rx = max(0, self.getAlpha() - a * W)
+        sx = max(0, self.getBeta() - (1 - a) * W)
+        return Opinion((rx / (rx + sx + W)), (sx / (rx + sx + W)), (W / (rx + sx + W)), a)
